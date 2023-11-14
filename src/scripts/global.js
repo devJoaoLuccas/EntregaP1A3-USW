28f20c79ff00d82b380d9b7f9695c5ba86014c36
João Luccas <101368682+devJoaoLuccas@users.noreply.github.com>
2023-11-14 18:40:18 -0300

    feat: add src

---
+++ src/scripts/global.js
@@ -0,0 +1,9 @@
+document.getElementById("cadastre-se-link").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href = "/frontend-prototipo/src/pages/telaCadastroUsuario.html"
+}); 
+
+document.getElementById("login-link").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    window.location.href = "/frontend-prototipo/src/pages/index-menu.html"
+}); 
